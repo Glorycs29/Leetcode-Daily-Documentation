fce20d30a5dc5a73ecc50986f209d3ea024328ed
Glory Gupta <prikarshglory@gmail.com>
2023-09-11 22:18:19 +0530

    Time: 19 ms (86.53%), Space: 15.6 MB (64.78%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,35 +1,62 @@
-class Solution {
+class DSU{
+private:
+
+    int *parent;
+    int *size;
+
 public:
-    void bfs(int node, vector<bool> &visited, vector<vector<int>>& isConnected){
-        queue<int> q;
-        q.push(node);
-        visited[node]=true;
 
-        while(!q.empty()){
-            node = q.front();
-            q.pop();
-
-            for(int i=0; i<isConnected.size(); i++){
-
-                if(isConnected[node][i] && !visited[i]){
-                    q.push(i);
-                    visited[i]=true;
-                }
-            }
+    DSU(int v){
+        size = new int[v];
+        parent = new int[v];
+        for(int i = 0; i < v; i++){
+            parent[i] = i;
+            size[i] = 1;
         }
     }
 
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int v = isConnected.size();
-        int nComp=0;
-        vector<bool> visited(v,false);
+    int Find(int node){
+        if(parent[node] != node){
+            return parent[node] = Find(parent[node]); // path compression step.
+        }
+        return node;
+    }
 
-        for(int i=0; i<v; i++){
-            if(!visited[i]){
-                bfs(i,visited,isConnected);
-                nComp++;
+    void Union(int u,int v){
+        int up = Find(u);
+        int vp = Find(v);
+        if(up == vp) return;
+        if(size[up] < size[vp]){
+            parent[up] = vp;
+            size[vp] += size[up];
+        }
+        else{
+            parent[vp] = up;
+            size[up] += size[vp];
+        }
+    }
+
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& group) {
+        int n = group.size();
+        int nComp = 0;
+
+        DSU dsu(n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(group[i][j]){
+                    dsu.Union(i,j);
+                } 
             }
         }
+
+        for(int i=0; i<n; i++){
+            nComp += dsu.Find(i)==i;
+        }
         return nComp;
+
     }
 };
