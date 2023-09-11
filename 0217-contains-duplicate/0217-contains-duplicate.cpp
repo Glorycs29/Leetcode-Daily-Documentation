7b866c64d3a0e5fcf37eb79b3550cae983435cd7
Glory Gupta <prikarshglory@gmail.com>
2023-09-11 17:42:44 +0530

    Time: 89 ms (85.96%), Space: 57.4 MB (76.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,26 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//          // Find the intersection point of the two runners.
+//         set<int> s;
+//         for(auto x:nums){
+//             if(s.find(x)!=s.end())return true;
+//             else s.insert(x);
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-         // Find the intersection point of the two runners.
-        set<int> s;
-        for(auto x:nums){
-            if(s.find(x)!=s.end())return true;
-            else s.insert(x);
+        sort(nums.begin(), nums.end());
+
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i-1] == nums[i]) return true;
         }
+
         return false;
     }
 };
