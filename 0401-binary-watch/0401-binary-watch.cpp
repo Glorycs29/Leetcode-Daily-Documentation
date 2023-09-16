17287dbc9c529cd70621f35dd8ed7168b98ebc2e
Glory Gupta <prikarshglory@gmail.com>
2023-09-16 16:48:50 +0530

    Time: 3 ms (47.26%), Space: 6.7 MB (19.45%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.cpp
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
-    vector<string> readBinaryWatch(int num) {
-    vector<string> rs;
-    for (int h = 0; h < 12; h++)
-    for (int m = 0; m < 60; m++)
-        if (bitset<10>(h << 6 | m).count() == num)
-            rs.emplace_back(to_string(h) + (m < 10 ? ":0" : ":") + to_string(m));
-    return rs;
+    vector<string> readBinaryWatch(int num) {   // let num be 6
+    int mask = 1 << 10;                     // mask = "10000000000
+    vector<string>ans;
+    for (int i = 0; i < mask; i++)
+    {
+        if (__builtin_popcount(i) == num)   // counts number of set bits
+        {                                   // let i = "101001111"
+            int hr = i >> 6;                // hours = "101" = 5
+            int mn = (hr << 6)^i;           // hr<<6 ="101000000"  
+                                            // xor of i and hr<<6 will give us minutes 
+			if (hr < 12 && mn < 60)
+            {
+                ans.push_back(to_string(hr) + ((mn < 10) ? ":0" : ":") + to_string(mn));
+            }
+        }
+    }
+    return ans;
+
     }
 };
