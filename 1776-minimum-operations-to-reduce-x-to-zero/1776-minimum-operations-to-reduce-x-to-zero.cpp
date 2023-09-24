efb1ceeee64c0730fb76af8a0da907038b9ba0d4
Glory Gupta <prikarshglory@gmail.com>
2023-09-24 09:38:36 +0530

    Time: 117 ms (84.84%), Space: 98.9 MB (38.32%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int minOperations(vector<int>& v, int x) {
         
-        int i, len=0, sum1=0, sum2=0;
+        int i, len=0, sum1=0, target=-x;
         for(i=0; i<v.size(); i++)
-            sum2+=v[i];
+            target+=v[i];
 
-        sum2-=x;
-
-        if(sum2==0)
+        if(target==0)
             return v.size();
         int j; i=0;
         for(j=0; j<v.size(); j++){
             sum1+=v[j];
 
-            while(i<v.size() && sum1>sum2)
+            while(i<v.size() && sum1>target)
                 sum1-=v[i++];
 
-            if(sum1==sum2)
+            if(sum1==target)
                 len=max(len,j-i+1);
         }
         if(!len)
