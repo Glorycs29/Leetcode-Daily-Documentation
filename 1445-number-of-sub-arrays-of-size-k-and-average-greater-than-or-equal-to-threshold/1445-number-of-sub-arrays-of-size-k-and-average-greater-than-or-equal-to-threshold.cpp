dfab53b91466d6161a42fd80846561ee112b51b6
Glory Gupta <prikarshglory@gmail.com>
2023-09-25 14:28:30 +0530

    Time: 69 ms (40.57%), Space: 55.8 MB (6.41%) - LeetHub

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        ios_base::sync_with_stdio;
+        cin.tie(NULL);
+        cout.tie(NULL);
+        
         int n=arr.size(), sum=0, cnt=0;
         for(int i=0;i<k; i++) sum+=arr[i];
         if(sum/k >= threshold) cnt++;
