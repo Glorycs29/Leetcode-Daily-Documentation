6dcd4fa91620700900cf1b178761fa619c970b5f
Glory Gupta <prikarshglory@gmail.com>
2023-09-25 14:16:25 +0530

    Time: 93 ms (99.09%), Space: 110 MB (14.72%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        double max_avg=INT_MIN, sum=0;
-        int n=nums.size();
-        for(int i=0; i<k; i++){
-            sum+=nums[i];
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+        int sum = 0;
+        for (int i = 0; i < k; ++i) sum += nums[i];
+
+        int max_sum = sum;
+        for (int i = k; i < nums.size(); ++i) {
+            sum += nums[i] - nums[i - k];
+            max_sum = max(sum, max_sum);
         }
-        max_avg=sum;
-        for(int i=0; i<n-k; i++){
-            sum+=nums[i+k]-nums[i];
-            max_avg = max(max_avg, sum);
-        }
-        return max_avg/k;
+
+        return static_cast<double>(max_sum) / k;
     }
 };
