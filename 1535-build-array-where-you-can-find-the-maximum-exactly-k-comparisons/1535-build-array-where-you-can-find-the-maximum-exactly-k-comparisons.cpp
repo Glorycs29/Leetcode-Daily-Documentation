6887501a8f4b18224ae812b79c52e7b2104e304c
Glory Gupta <prikarshglory@gmail.com>
2023-10-07 19:07:30 +0530

    Time: 20 ms (86.18%), Space: 7.3 MB (71.89%) - LeetHub

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    /* let ways[i][j][k] = # ways to construct an array of length i with max element equal to j and a search cost of k. */
-    long long ways[51][101][51];
-    const int MOD = 1e9 + 7;
-    
     int numOfArrays(int n, int m, int k) {
-		/* There are our base cases. For each index 1 <= j <= m, we require ways[1][j][1] = 1 because the array consisting of only the element j
-			has length 1, maximum element j, and it has a search cost of 1. */
-        for (int j = 1; j <= m; j++) {
-                ways[1][j][1] = 1;
-        }
-        
-        for (int a = 1; a <= n; a++) {
-            for (int b = 1; b <= m; b++) {
-                for (int c = 1; c <= k; c++) {
-                    long long s = 0;
+        const int mod = 1e9 + 7;
 
-		            /* In this first case, we can append any element from [1, b] to the end of the array. */
-                    s = (s + b * ways[a - 1][b][c]) % MOD;
+        vector<vector<int>> dp(m+1, vector<int>(k+1, 0));
+        vector<vector<int>> prefix(m+1, vector<int>(k+1, 0));
+        vector<vector<int>> prevDp(m+1, vector<int>(k+1, 0));
+        vector<vector<int>> prevPrefix(m+1, vector<int>(k+1, 0));
+
+        for (int j = 1; j <= m; j++) {
+            prevDp[j][1] = 1;
+            prevPrefix[j][1] = j;
+        }
+
+        for (int _ = 2; _ <= n; _++) {
+            dp.assign(m+1, vector<int>(k+1, 0));
+            prefix.assign(m+1, vector<int>(k+1, 0));
+
+            for (int maxNum = 1; maxNum <= m; maxNum++) {
+                for (int cost = 1; cost <= k; cost++) {
+                    dp[maxNum][cost] = (static_cast<long long>(maxNum) * prevDp[maxNum][cost]) % mod;
                     
-		           /* In this second case, we can append the element "b" to the end of the array. */
-                    for (int x = 1; x < b; x++) {
-						s = (s + ways[a - 1][x][c - 1]) % MOD;
+                    if (maxNum > 1 && cost > 1) {
+                        dp[maxNum][cost] = (dp[maxNum][cost] + prevPrefix[maxNum - 1][cost - 1]) % mod;
                     }
-					
-                    ways[a][b][c] = (ways[a][b][c] + s) % MOD;
+                    
+                    prefix[maxNum][cost] = (prefix[maxNum - 1][cost] + dp[maxNum][cost]) % mod;
                 }
             }
+
+            prevDp = dp;
+            prevPrefix = prefix;
         }
 
-        long long ans = 0;
-        for (int j = 1; j <= m; j++) {
-            ans = (ans + ways[n][j][k]) % MOD;
-        }
-        
-        return int(ans);
+        return prefix[m][k];
     }
 };
