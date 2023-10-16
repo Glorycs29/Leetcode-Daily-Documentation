b24abedd9faa08c5819554296feff8c2dd94a04f
Glory Gupta <prikarshglory@gmail.com>
2023-10-16 14:28:01 +0530

    Time: 3 ms (36.93%), Space: 6.8 MB (35.86%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    vector<int> getRow(int rowIndex) {
-        vector<int> pascalRow(rowIndex+1, 1);
 
-        for(int i=0; i<=rowIndex; i++){
-            for(int j=i-1; j>0; j--){
-                pascalRow[j] = pascalRow[j] + pascalRow[j-1];
-            }
+    long long int ncr(int n, int r){
+        long long int ans =1;
+        for(int col=0; col<r; col++){
+            ans = ans*(n-col);
+            ans = ans/(col+1);
         }
-        return pascalRow;
-    } 
+        return ans;
+    }
+
+    vector<int> getRow(int rowIndex) {
+        vector<int> res;
+        for(int i=0; i<=rowIndex; i++){
+            res.push_back(ncr(rowIndex, i));
+        }
+        return res;
+    }
 };
